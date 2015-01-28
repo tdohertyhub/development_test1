d33927aefd30c2d3a1dc86ad80583327fcbb0eb6
tdoherty65 <tdoherty@fastmail.fm>
2015-01-28 17:21:16 +0000

    1
    (cherry picked from commit aa7999dcd6559687c39fde7e47caaf17e0626774)

--- 1.php
+++ 1.php
@@ -1,9 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Thomas
- * Date: 28/01/2015
- * Time: 16:55
- */
-echo "tom lives here";
+for ($i=0; $i<5;$i++){
+    echo "Helloworld<br/>";
+}
+?>
+    <hr/>
+
+<?php
+$x=5;
+if($x == 4)
+    echo "x is 4";
+else
+    echo "x is not 4";
+?>
+
+    <hr/>
+
+<?php
+$arr = array();
+$arr[] = "dog";
+$arr[] = "cat";
+//var_dump($arr); //print out entire contents of array
+
+for ($i=0; $i< count($arr);$i++){
+    echo $i."=".$arr[$i]."<br/>";
+}
+?>
+    <hr/>
+
+<?php
+function println ($str){
+    echo $str."<br/>";
+}
+
+println("my");
+println("name");
+println("is");
+println("tom");
+
+?>
+
+    <hr/>
+
+    <a href="1.php? x=1">1 time </a><br/>
+    <a href="1.php? x=8">8 times </a><br/>
+    <hr/>
+
+<?php
+if (isset($_GET['x']))
+    $x=$_GET['x'];
+else
+    $x=0;
+
+for ($i=0;$i<$x;$i++)
+    println("row #".$i);
+
 ?>
